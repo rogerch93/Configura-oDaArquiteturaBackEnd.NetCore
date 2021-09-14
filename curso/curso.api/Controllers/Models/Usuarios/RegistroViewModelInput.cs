64ee0a3b262bb75bf058a92ec7fdbc6503d36302
rogerch93 <rogerinhochaves@hotmail.com>
2021-09-14 10:23:49 -0300

    inicio do curso

---
+++ curso/curso.api/Controllers/Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace curso.api.Controllers.Models.Usuarios
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O E-Mail é obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A Senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
---
+++ curso/curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using curso.api.Controllers.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    { 
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("Registrar")]
+        public IActionResult Registrar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
